bc372d1b7286a807feb3fae3350c4a1f25e7e1fc
jha876622 <jha876622@gmail.com>
2026-04-08 00:04:50 +0530

    fix model resave

--- backend/fix_model.py
+++ backend/fix_model.py
@@ -1,9 +1,5 @@
 import tensorflow as tf
 
-# Load old model
 model = tf.keras.models.load_model("model/mask_detector.h5")
-
-# Re-save in new format
-model.save("model/mask_detector.h5", save_format="h5")
-
-print("Model re-saved successfully!")
+model.save("model/mask_detector.h5")
+print("Done!")
